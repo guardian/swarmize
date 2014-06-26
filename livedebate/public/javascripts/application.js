d12495907b1d5bcf60bb77793911904b332f3252
Tom Armitage <tom@infovore.org>
2014-06-26 10:52:48 +0100

    Doing some mobilelayout stuff.

--- livedebate/public/javascripts/application.js
+++ livedebate/public/javascripts/application.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
 
 	// Open external links in a new window
-	hostname = window.location.hostname
-	$("a[href^=http]")
-	  .not("a[href*='" + hostname + "']")
-	  .addClass('link external')
-	  .attr('target', '_blank');
-
-
   $(".feedback-buttons").hide();
 
   $(".next-button").click(function(e) {
     $(".setup").hide();
+    $("h1").hide();
     $(".feedback-buttons").show();
     e.preventDefault();
   });
 
   $(".feedback-button").click(function(e) {
-    $("input#feedback").val(this.id);
-    $.post( $("form#debate").attr('action'),
-           $("form#debate").serialize());
+    if(!$(this).hasClass('disabled')) {
+      fadeOutLinks();
+      $("input#feedback").val(this.id);
+      $.post( $("form#debate").attr('action'),
+             $("form#debate").serialize());
+    }
     e.preventDefault();
   });
 
 });
+
+function fadeOutLinks() {
+  $(".feedback-button").animate({'opacity': 0.3},500);
+  $(".feedback-button").addClass('disabled');
+  setTimeout(fadeUpLinks, 3000);
+}
+
+function fadeUpLinks() {
+  $(".feedback-button").animate({'opacity': 1},500);
+  $(".feedback-button").fadeTo(1);
+  $(".feedback-button").removeClass('disabled');
+}
