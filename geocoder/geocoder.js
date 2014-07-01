e06ca1b6520f8581025fc0b4f4418f7ec21f3cc6
Tom Armitage <tom@infovore.org>
2014-07-01 10:35:44 +0100

    Standardising input string for these.

--- geocoder/geocoder.js
+++ geocoder/geocoder.js
@@ -5,7 +5,7 @@ var app = express();
 app.get("/geocode", function(req,res) {
   console.log(req);
 
-  request('http://open.mapquestapi.com/nominatim/v1/search.php?format=json&limit=5&q='+req.query.address, function (error, response, body) {
+  request('http://open.mapquestapi.com/nominatim/v1/search.php?format=json&limit=5&q='+req.query.input, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var json = JSON.parse(body);
       res.json(json);
