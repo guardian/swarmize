c5959ba43ca3a345238bb3d0ee974273f1ef3822
Tom Armitage <tom@infovore.org>
2014-07-08 10:35:25 +0100

    Basic packaging script.

---
+++ mock-website/package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CURRENTREV=$(git log --pretty=format:'%h' -n 1)
+pushd . > /dev/null
+cd ..
+echo "Archiving website."
+git archive -o mock-website/package-$CURRENTREV.zip master:mock-website
+popd > /dev/null
+
+#aws s3 cp target/universal/swarmize-collector.tgz s3://swarmize-dist/collector.tar.gz --acl public-read
+#aws s3 cp collector.conf s3://swarmize-dist/collector.conf --acl public-read
+
