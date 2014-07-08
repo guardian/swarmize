319a9ad0914d2b8e343d1e870b886ddba2622674
Tom Armitage <tom@infovore.org>
2014-07-08 22:52:39 +0100

    Fixing this according to shellcheck.com

--- mock-website/deploy.sh
+++ mock-website/deploy.sh
@@ -6,17 +6,17 @@ CURRENTREV=$(git log --pretty=format:'%h' -n 1)
 pushd . > /dev/null
 cd ..
 echo "Archiving website to package-$CURRENTREV.zip"
-git archive -o mock-website/package-$CURRENTREV.zip master:mock-website
+git archive -o "mock-website/package-$CURRENTREV.zip" master:mock-website
 popd > /dev/null
 
 echo "Uploading to S3."
 
-aws s3 cp package-$CURRENTREV.zip s3://swarmize-demo/package-$CURRENTREV.zip
+aws s3 cp "package-$CURRENTREV.zip" "s3://swarmize-demo/package-$CURRENTREV.zip"
 
 echo "Creating new application version"
 
-aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label package-$CURRENTREV --source-bundle S3Bucket=swarmize-demo,S3Key=package-$CURRENTREV.zip
+aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label "package-$CURRENTREV" --source-bundle "S3Bucket=swarmize-demo,S3Key=package-$CURRENTREV.zip"
 
 echo "Deploying application version"
 
-aws elasticbeanstalk update-environment --environment-name 'Swarmize-E2E-Llive' --version-label package-$CURRENTREV
+aws elasticbeanstalk update-environment --environment-name 'Swarmize-E2E-Llive' --version-label "package-$CURRENTREV"
