e478f95c0546de58d5da47979dc5284df83d86d7
Tom Armitage <tom@infovore.org>
2014-07-08 12:44:49 +0100

    First pass at integrating Elasticsearch. It's not horrible.

--- mock-website/app.rb
+++ mock-website/app.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 require 'sinatra/base'
 require 'elasticsearch'
+require 'jbuilder'
+require 'hashie'
 
 class MockSwarmizeWebsite < Sinatra::Base
 
@@ -8,9 +10,9 @@ class MockSwarmizeWebsite < Sinatra::Base
   swarm = Swarm.new("abc123", "Live Debate Swarm")
 
   # eg
-  client = Elasticsearch::Client.new log: true, host: 'hostname.com:9200'
-  client = Elasticsearch::Client.new log: true
-  
+  client = Elasticsearch::Client.new log: true, host: 'ec2-54-83-167-14.compute-1.amazonaws.com:9200'
+
+
   get '/' do
     haml :index, :locals => {:swarms => [swarm]}
   end
@@ -22,9 +24,40 @@ class MockSwarmizeWebsite < Sinatra::Base
   end
 
   get '/swarms/:key/data' do
-    # static page
+    if params[:page]
+      page = params[:page].to_i
+    else
+      page = 1
+    end
 
-    haml :data, :locals => {:swarm => swarm}
+    query = Jbuilder.encode do |json|
+      json.size 10
+      json.from 10 * (page-1)
+      json.query do
+        json.filtered do
+          json.filter do
+            json.terms do
+              json.feedback %w{lab con ld}
+            end
+          end
+        end
+      end
+      json.sort do
+        json.timestamp "asc"
+      end
+    end
+
+    search_results = client.search(index: 'voting', body: query)
+
+    search_results_hash = Hashie::Mash.new(search_results)
+
+    total_hits = search_results_hash.hits.total
+    per_page = 10
+    total_pages = (total_hits/per_page) + 1
+
+    rows = search_results_hash.hits.hits.map {|h| h._source}
+
+    haml :data, :locals => {:swarm => swarm, rows: rows, current_page: page, total_pages: total_pages}
   end
 
   # start the server if ruby file executed directly
