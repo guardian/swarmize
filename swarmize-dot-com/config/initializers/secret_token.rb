5e4ec5043d9ed69050d5a24ad301ff807a4070b0
Tom Armitage <tom@infovore.org>
2014-07-12 10:42:35 +0100

    That's your basic shell of a public website, I think.

---
+++ swarmize-dot-com/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swarmize::Application.config.secret_key_base = 'dbe967e6beee21a22e385dac263b7636e6f6069a144d4993a0641f66183fa4507322439902e4a3530db95d2acd70a853a315d6347981f16fc1902a5bb5940d47'
