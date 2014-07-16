48650967ca26553f5e07ffc96b7bd946e7459656
Tom Armitage <tom@infovore.org>
2014-07-16 10:11:03 +0100

    Tidying, and work in progress.

---
+++ swarmize-dot-com/app/models/form_builder_field.rb
@@ -0,0 +1,17 @@
+class FormBuilderField
+  ALL = { :text => "Text",
+          :bigtext => "Big Text",
+          :address => "Address",
+          :city => "City",
+          :county => "County",
+          :state => "State",
+          :country => "Country",
+          :email => "Email",
+          :number => "Number",
+          :pick_one => "Pick One",
+          :pick_several => "Pick Several",
+          :rating => "Rating",
+          :yesno => "Yes/No",
+          :checkbox => "Checkbox"
+  }
+end
