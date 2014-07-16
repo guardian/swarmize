c3a78009b695dcdb25319136a3fef80d991bd47a
Tom Armitage <tom@infovore.org>
2014-07-16 17:53:44 +0100

    That's how to save it for country.

--- swarmize-dot-com/app/controllers/swarms_controller.rb
+++ swarmize-dot-com/app/controllers/swarms_controller.rb
@@ -1,14 +1,14 @@
 class SwarmsController < ApplicationController
+  before_filter :scope_to_swarm, :except => %w{index new create}
+
   def index
     @swarms = Swarm.all
   end
 
   def show
-    @swarm = Swarm.find(params[:id])
   end
 
   def fields
-    @swarm = Swarm.find(params[:id])
   end
 
   def new
@@ -21,11 +21,15 @@ class SwarmsController < ApplicationController
   end
 
   def update_fields
-    render :text => params.inspect
-
+    @swarm.update(:fields => params[:fields])
+    redirect_to fields_swarm_path(@swarm)
   end
 
   private
+
+  def scope_to_swarm
+    @swarm = Swarm.find(params[:id])
+  end
 
   def swarm_params
     params.require(:swarm).permit(:name, :description)
