4c22acfb60becd6706837a9f42440b7ac58a2555
Tom Armitage <tom@infovore.org>
2014-07-18 11:11:28 +0100

    Default route, for now.

--- swarmize-dot-com/config/routes.rb
+++ swarmize-dot-com/config/routes.rb
@@ -1,4 +1,6 @@
 Swarmize::Application.routes.draw do
+  root "swarms#index"
+
   resources :swarms do
     member do
       get 'fields'
@@ -6,39 +8,6 @@ Swarmize::Application.routes.draw do
       post 'update_fields'
     end
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
