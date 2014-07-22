1fb480fdbac016a49e3b24e6e78f301f9069c64e
Tom Armitage <tom@infovore.org>
2014-07-22 17:26:48 +0100

    Tidying routes file.

--- swarmize-dot-com/config/routes.rb
+++ swarmize-dot-com/config/routes.rb
@@ -18,25 +18,4 @@ Swarmize::Application.routes.draw do
 
   get 'results/:swarm_id/graphs/count/:count_field', to: 'graphs#aggregate_count'
   get 'results/:swarm_id/graphs/count/:count_field/:unique_field', to: 'graphs#cardinal_count'
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
