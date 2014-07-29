dfe8079a408c64ad3b18c545c54c290743eecd86
Tom Armitage <tom@infovore.org>
2014-07-29 11:45:46 +0100

    Adding redirect_back_or_default

--- swarmize-dot-com/app/controllers/application_controller.rb
+++ swarmize-dot-com/app/controllers/application_controller.rb
@@ -6,7 +6,40 @@ class ApplicationController < ActionController::Base
 
   def set_up_current_user
     if session[:user_id]
-      @current_user = User.first(session[:user_ud])
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def check_for_user
+    if !logged_in?
+      store_location
+      redirect_to login_path
+    end
+  end
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def login_path
+    "/auth/google_oauth2"
+  end
+
+  def store_location
+    if request.get?
+      session[:return_to] = request.original_url
+    else
+      session[:return_to] = request.referer
+    end
+  end
+
+  def redirect_back_or_default(default = root_url)
+    if session[:return_to]
+      redirect_url = session[:return_to]
+      session.delete(:return_to)
+      redirect_to(redirect_url)
+    else
+      redirect_to(default)
     end
   end
 end
