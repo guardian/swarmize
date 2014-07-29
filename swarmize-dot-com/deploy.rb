7f833772f588498c7d3d498120907d74567c43e6
Tom Armitage <tom@infovore.org>
2014-07-29 12:05:37 +0100

    Adding slack notification to deployment.

--- swarmize-dot-com/deploy.rb
+++ swarmize-dot-com/deploy.rb
@@ -2,7 +2,15 @@
 
 require 'csv'
 require 'fog'
+require 'httparty'
 require './fogpatch.rb' # patch for Elastic Beanstalk errors.
+
+def notify_slack(version)
+  slack_url = "https://swarmize.slack.com/services/hooks/incoming-webhook?token=N9AEOcz68MVeKgk105K9WRld"
+  host = "http://swarmize-prod.elasticbeanstalk.com"
+
+  HTTParty.post(slack_url, body: {channel: '#general', username: 'deploybot', text: "Deployed version #{version} to <#{host}>"}.to_json)
+end
 
 # stick your standard Amazon creds in the main project directory.
 creds = CSV.read('../credentials.csv', :headers => true)[0]
@@ -68,6 +76,7 @@ else
     :source_bundle => {"S3Bucket" => S3_BUCKET,
                        "S3Key" => package_filename}
   )
+
 end
 
 live_environment = application.environments.find {|e| e.name == LIVE_ENV }
@@ -76,6 +85,7 @@ if live_environment.version_label != package_version
   puts "Creating new application version #{package_version} on environment #{LIVE_ENV}."
 
   live_environment.version = version
+  notify_slack(package_version)
 else
   puts "Environment #{LIVE_ENV} already running #{package_version}."
 end
