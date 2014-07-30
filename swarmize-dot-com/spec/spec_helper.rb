d0a368d002a04eaadd58bbe8a4d0a21737d767c3
Tom Armitage <tom@infovore.org>
2014-07-30 12:02:04 +0100

    Timecop.

--- swarmize-dot-com/spec/spec_helper.rb
+++ swarmize-dot-com/spec/spec_helper.rb
@@ -58,4 +58,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:each) do
+    Timecop.return # Timecop should reset before each test is run.
+  end
 end
+
