93636c191e31d462ee8829396c3ee739b7a6b856
Tom Armitage <tom@infovore.org>
2014-07-30 16:16:53 +0100

    Logged in / logged out home views.

--- swarmize-dot-com/app/controllers/home_controller.rb
+++ swarmize-dot-com/app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def show
+    if @current_user
+      @user_swarms_yet_to_launch = @current_user.swarms.yet_to_launch.latest(5)
+      @user_swarms_in_progress = @current_user.swarms.live.latest(5)
+      @user_completed_swarms = @current_user.swarms.closed.latest(5)
+    end
     @swarms_in_progress = Swarm.live.latest(5)
     @latest_complete_swarms = Swarm.closed.latest(5)
   end
