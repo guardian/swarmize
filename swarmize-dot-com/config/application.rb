9f2369e3e2dcd2ddfcb00088c1556598ae395b8e
Tom Armitage <tom@infovore.org>
2014-08-01 12:30:23 +0100

    Fixing webfonts.

--- swarmize-dot-com/config/application.rb
+++ swarmize-dot-com/config/application.rb
@@ -28,5 +28,7 @@ module Swarmize
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths << Rails.root.join('lib')
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    
   end
 end
