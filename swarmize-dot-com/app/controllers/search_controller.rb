36bb0fa31f744371d7c58b792899e4eaf5a6cd6b
Tom Armitage <tom@infovore.org>
2014-08-01 15:56:40 +0100

    And search for unspiked swarms, too.

--- swarmize-dot-com/app/controllers/search_controller.rb
+++ swarmize-dot-com/app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def results
     @query = params[:query]
-    @results = Swarm.search_by_name_and_description(@query)
+    @results = Swarm.unspiked.search_by_name_and_description(@query)
   end
 end
