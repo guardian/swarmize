3a58c2236d20e42fe39eed08548147febd20640e
Tom Armitage <tom@infovore.org>
2014-08-19 13:01:56 +0100

    Processor deployment tool.

--- build-processor.sh
+++ build-processor.sh
@@ -8,7 +8,7 @@ APP_NAME=processor
 # build the zip
 sbt ${APP_NAME}/docker:stage && \
   cd ${APP_NAME}/target/docker && \
-  zip -r ../../../${APP_NAME}.zip *
+  zip -r ../../${APP_NAME}.zip *
 
 echo "I've built ${APP_NAME}.zip as a docker image. Now upload this as new version to elasticbeanstalk, and deploy"
 
