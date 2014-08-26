6efbb244e3d71c3f17d3411f51048c3cc96728a6
Tom Armitage <tom@infovore.org>
2014-08-26 16:56:12 +0100

    You should only be able to have one AccessPermission per email.

--- swarmize-dot-com/app/models/access_permission.rb
+++ swarmize-dot-com/app/models/access_permission.rb
@@ -2,6 +2,8 @@ class AccessPermission < ActiveRecord::Base
   belongs_to :swarm
   belongs_to :creator, :class_name => 'User', :foreign_key => 'user_id'
 
+  validates_uniqueness_of :email
+
   def user
     User.where(email: email).first
   end
