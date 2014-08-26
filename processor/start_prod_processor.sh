e7691f9956e951a162c4a8dc73b2fe21507bab93
Graham Tackley <graham@tackley.net>
2014-08-26 15:28:48 +0100

    Simple command line so I can force my local processor to be the only one running

---
+++ processor/start_prod_processor.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+aws --profile swarmize autoscaling update-auto-scaling-group \
+  --auto-scaling-group-name awseb-e-6bv6raqwgv-stack-AWSEBAutoScalingGroup-18Q0FOJPKDO5R \ 
+  --min-size 1 \
+  --desired-capacity 1
