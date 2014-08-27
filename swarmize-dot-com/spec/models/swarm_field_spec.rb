24833cfa6396d72108698b127a9f9c69bacbb3f5
Tom Armitage <tom@infovore.org>
2014-08-27 12:34:10 +0100

    Adding a spec for this.

--- swarmize-dot-com/spec/models/swarm_field_spec.rb
+++ swarmize-dot-com/spec/models/swarm_field_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe SwarmField do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'being created' do
+    it 'should set its field_code correctly' do
+      s = SwarmField.create(:field_name => 'What is your postcode?')
+      expect(s.field_code).to eq('what_is_your_postcode')
+    end
+  end
 end
