d6ace28185ec33fe6b29d24ec120a3762b5738f0
Tom Armitage <tom@infovore.org>
2014-08-27 12:34:16 +0100

    Removing pending specs.

--- swarmize-dot-com/spec/helpers/permissions_helper_spec.rb
+++ swarmize-dot-com/spec/helpers/permissions_helper_spec.rb
@@ -1,15 +1,3 @@
 require 'spec_helper'
-
-# Specs in this file have access to a helper object that includes
-# the PermissionsHelper. For example:
-#
-# describe PermissionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe PermissionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
