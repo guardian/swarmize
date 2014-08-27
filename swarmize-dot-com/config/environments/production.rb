b1f05b43eb75c9faace465fd8af68c3c6dbc9be2
Tom Armitage <tom@infovore.org>
2014-08-27 17:59:49 +0100

    Amazon SES used for email delivery.

--- swarmize-dot-com/config/environments/production.rb
+++ swarmize-dot-com/config/environments/production.rb
@@ -77,4 +77,7 @@ Swarmize::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :ses
+  
 end
---
+++ swarmize-dot-com/config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
