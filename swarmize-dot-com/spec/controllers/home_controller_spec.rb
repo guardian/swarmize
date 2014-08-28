68b3328b10e74d1cc9016b813c398818eb951ad1
Tom Armitage <tom@infovore.org>
2014-08-28 12:43:57 +0100

    Testing a controller.

--- swarmize-dot-com/spec/controllers/home_controller_spec.rb
+++ swarmize-dot-com/spec/controllers/home_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
+
+  describe "GET #show" do
+    describe "when logged out" do
+      it "renders the show page" do
+        get :show
+        expect(response).to render_template :show
+      end
+
+      it "renders the logged out partial" do
+        get :show
+        expect(response).to render_template(:partial => "home/_logged_out")
+      end
+
+    end
+
+    describe "when logged in" do
+      before do
+        user = Factory(:user)
+        session[:user_id] = user.id
+      end
+
+      it "renders the logged in partial" do
+        get :show
+        expect(response).to render_template(:partial => "home/_logged_in")
+      end
+    end
+
+  end
 
 end
