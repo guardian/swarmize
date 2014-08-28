a369717ea6787345d34a89c60feafb392f5b0ac9
Tom Armitage <tom@infovore.org>
2014-08-28 14:57:55 +0100

    use routes in testing.

--- swarmize-dot-com/spec/spec_helper.rb
+++ swarmize-dot-com/spec/spec_helper.rb
@@ -17,6 +17,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
