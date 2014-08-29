4b6bf5aa9d2dbf61522b73786a2fa421c400490a
Graham Tackley <graham@tackley.net>
2014-08-29 11:49:54 +0100

    Test: implement the todo on the tests

--- shared-lib/src/test/scala/swarmize/json/SubmittedDataTest.scala
+++ shared-lib/src/test/scala/swarmize/json/SubmittedDataTest.scala
@@ -2,7 +2,7 @@ package swarmize.json
 
 import org.scalatest._
 import play.api.libs.json.Json
-import swarmize.Swarm
+import swarmize.{TestSwarms, Swarm}
 
 class SubmittedDataTest extends FlatSpec with Matchers with OptionValues {
 
@@ -14,18 +14,15 @@ class SubmittedDataTest extends FlatSpec with Matchers with OptionValues {
 
     val submissionJson = Json.parse(rawSubmission)
 
-    // TODO: create a way to create dummy config for testing
-//    val swarmConfig = SwarmConfig.findByToken("voting").value
-//
-//    val wrapped = SubmittedData.wrap(submissionJson, swarmConfig, List("SomeProcessingStep"))
-//
-//    val id = wrapped.submissionId
-//
-//    Json.toJson(wrapped).toString shouldBe
-//      s"""{"submissionId":"$id","swarmName":"Voting Intentions","swarmToken":"voting",
-//         |"processingSteps":["SomeProcessingStep"],
-//         |"data":{"user_key":12345,"timestamp":1407228014,"intent":"green","feedback":"Mr King","postcode":"N1 9GU","ip":"10.0.0.1"}}"""
-//        .stripMargin.replace("\n", "")
+    val wrapped = SubmittedData.wrap(submissionJson, TestSwarms.simpleSurvey)
+
+    val id = wrapped.submissionId
+
+    Json.toJson(wrapped).toString shouldBe
+      s"""{"submissionId":"$id","swarmName":"A very simple survey","swarmToken":"test-simple",
+         |"processingSteps":[],
+         |"data":{"user_key":12345,"timestamp":1407228014,"intent":"green","feedback":"Mr King","postcode":"N1 9GU","ip":"10.0.0.1"}}"""
+        .stripMargin.replace("\n", "")
 
   }
 
