103cfb09643ec5d5d715ee472a09408e04a3e458
Tom Armitage <tom@infovore.org>
2014-08-29 10:49:15 +0100

    Adding coverage tools.

--- swarmize-dot-com/spec/spec_helper.rb
+++ swarmize-dot-com/spec/spec_helper.rb
@@ -2,6 +2,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'simplecov'
+
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
