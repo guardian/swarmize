13a75a5067016defd0f18e0a70c3c508f749846b
Tom Armitage <tom@infovore.org>
2014-09-04 11:16:24 +0100

    Default replyto should be Graham's.

--- swarmize-dot-com/app/mailers/permission_mailer.rb
+++ swarmize-dot-com/app/mailers/permission_mailer.rb
@@ -1,5 +1,6 @@
 class PermissionMailer < ActionMailer::Base
-  default from: "noreply@swarmize.com"
+  default from: "noreply@swarmize.com",
+          reply_to: 'graham.tackley@guardian.co.uk'
 
   def permission_email(to_address, swarm)
     @swarm = swarm
