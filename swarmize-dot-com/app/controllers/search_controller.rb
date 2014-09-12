725f71c7bdf8f1c3a90f214a67a3bb28868a84d7
Tom Armitage <tom@infovore.org>
2014-09-12 10:24:08 +0100

    Search can use the same partials as everybody else.

--- swarmize-dot-com/app/controllers/search_controller.rb
+++ swarmize-dot-com/app/controllers/search_controller.rb
@@ -2,9 +2,9 @@ class SearchController < ApplicationController
   def results
     @query = params[:query]
     if @current_user
-      @results = Swarm.unspiked.search_by_name_and_description(@query)
+      @swarms = Swarm.unspiked.search_by_name_and_description(@query)
     else
-      @results = Swarm.unspiked.publicly_visible.search_by_name_and_description(@query)
+      @swarms = Swarm.unspiked.publicly_visible.search_by_name_and_description(@query)
     end
   end
 end
