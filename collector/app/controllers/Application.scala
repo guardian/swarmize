b045a0f400ec181a3b8d0f9624457c13087a8c5a
Graham Tackley <graham@tackley.net>
2014-09-12 11:58:31 +0100

    Remove debug code

--- collector/app/controllers/Application.scala
+++ collector/app/controllers/Application.scala
@@ -1,13 +1,11 @@
 package controllers
 
 import play.api.mvc._
-import swarmize.FieldTypes
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(FieldTypes.json)
-    //Ok("swarmize: collector")
+    Ok("swarmize: collector")
   }
 
 }
