3494dd0f263b34bd79eb97bb13f24ed8574f9d42
Tom Armitage <tom@infovore.org>
2014-09-29 13:47:54 +0100

    Parsley validation for all field types.

---
+++ swarmize-dot-com/app/assets/javascripts/parsley-config.js
@@ -0,0 +1,21 @@
+window.ParsleyConfig = {
+  validators: {
+    postcode: {
+      fn: function (value, requirement) {
+        var fullPostcodeRegex = /^([A-Z][A-Z0-9]?[A-Z0-9]?[A-Z0-9]? {1,2}[0-9][A-Z0-9]{2})$/;
+        var halfPostcodeRegex = /^(([A-Z][0-9])|([A-Z][A-Z0-9][A-Z])|([A-Z][A-Z][0-9][A-Z0-9]?))$/;
+        if(value.trim().toUpperCase().match(fullPostcodeRegex) || value.trim().toUpperCase().match(halfPostcodeRegex)) {
+          return true;
+        } else {
+          return false
+        }
+      },
+      priority: 32
+    }
+  },
+  i18n: {
+    en: {
+      postcode: 'Please enter a valid UK postcode, eg "SW1A 1AA" or "SW1A"'
+    }
+  }
+};
--- swarmize-dot-com/app/helpers/fields_helper.rb
+++ swarmize-dot-com/app/helpers/fields_helper.rb
@@ -10,4 +10,52 @@ module FieldsHelper
     validations.join(",")
   end
 
+  def parsley_validations_for_field(field)
+    validations = {}
+    if field.compulsory
+      validations['data-parsley-required'] = true
+    end
+
+    case field.field_type
+    when 'url'
+      validations['data-parsley-type'] = 'url'
+    when 'number'
+      validations['data-parsley-type'] = 'number'
+    when 'postcode'
+      validations['data-parsley-postcode'] = true
+    when 'email'
+      validations['data-parsley-type'] = 'email'
+    when 'yesno'
+      validations['data-parsley-errors-container'] = "#errors_#{field.field_code}"
+      validations['data-parsley-error-message'] = "Please choose either yes or no."
+      validations['data-parsley-class-handler'] = "#group_#{field.field_code}"
+    when 'pick_one'
+      validations['data-parsley-errors-container'] = "#errors_#{field.field_code}"
+      validations['data-parsley-error-message'] = "Please pick one option from the list."
+      validations['data-parsley-class-handler'] = "#group_#{field.field_code}"
+    when 'rating'
+      validations['data-parsley-errors-container'] = "#errors_#{field.field_code}"
+      validations['data-parsley-error-message'] = "Please pick a rating from #{field.minimum} to #{field.maximum}."
+      validations['data-parsley-class-handler'] = "#group_#{field.field_code}"
+    when 'pick_several'
+      validations['data-parsley-errors-container'] = "#errors_#{field.field_code}"
+      validations['data-parsley-error-message'] = "Please pick at least one option from the list."
+      validations['data-parsley-class-handler'] = "#group_#{field.field_code}"
+    when 'check_box'
+      validations['data-parsley-errors-container'] = "#errors_#{field.field_code}"
+      validations['data-parsley-error-message'] = "Please check the checkbox."
+      validations['data-parsley-class-handler'] = "#group_#{field.field_code}"
+    end
+
+    if field.minimum
+      validations['data-parsley-min'] = field.minimum
+    end
+
+    if field.maximum
+      validations['data-parsley-max'] = field.maximum
+    end
+
+    validations
+  end
+
 end
