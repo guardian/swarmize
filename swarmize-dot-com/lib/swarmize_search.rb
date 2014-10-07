2a1697369ed4be91800e887fd168b8855d477e20
Tom Armitage <tom@infovore.org>
2014-10-07 15:15:34 +0100

    Catching errors when index doesn't exist.

--- swarmize-dot-com/lib/swarmize_search.rb
+++ swarmize-dot-com/lib/swarmize_search.rb
@@ -36,77 +36,103 @@ class SwarmizeSearch
 
   def count_all
     query = all_query(1, 10)
-    search_results_hash = search(token,query)
-    search_results_hash['hits']['total']
+
+    begin
+      search_results_hash = search(token,query)
+      search_results_hash['hits']['total']
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      0
+    end
   end
 
   def all(page=1, per_page=10)
     query = all_query(page, per_page)
-    
-    search_results_hash = search(token,query)
 
-    rows = search_results_hash.hits.hits.map {|h| h._source}
+    begin
+      search_results_hash = search(token,query)
 
-    total_pages = total_pages_for_results(search_results_hash, per_page)
+      rows = search_results_hash.hits.hits.map {|h| h._source}
 
-    [rows, total_pages]
+      total_pages = total_pages_for_results(search_results_hash, per_page)
+
+      [rows, total_pages]
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      [nil,0]
+    end
   end
 
   def latest
     query = latest_query
-    search_results_hash = search(token,query)
-    rows = search_results_hash.hits.hits.map {|h| h._source}
-    rows.first
+    begin
+      search_results_hash = search(token,query)
+      rows = search_results_hash.hits.hits.map {|h| h._source}
+      rows.first
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      nil
+    end
   end
 
   def entirety
-    results = []
-    # scan and scroll
-    # Open the "view" of the index with the `scan` search_type
-    r = @client.search index: @token,
-                      search_type: 'scan', 
-                      scroll: '1m', 
-                      size: 5000
+    begin
+      results = []
+      # scan and scroll
+      # Open the "view" of the index with the `scan` search_type
+      r = @client.search index: @token,
+                        search_type: 'scan', 
+                        scroll: '1m', 
+                        size: 5000
 
-    while r = @client.scroll(scroll_id: r['_scroll_id'], scroll: '1m') and not r['hits']['hits'].empty? do
-      results += r['hits']['hits'].map {|h| Hashie::Mash.new(h['_source'])}
+      while r = @client.scroll(scroll_id: r['_scroll_id'], scroll: '1m') and not r['hits']['hits'].empty? do
+        results += r['hits']['hits'].map {|h| Hashie::Mash.new(h['_source'])}
+      end
+      results
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      nil
     end
-    results
   end
 
   def aggregate_count(field)
     query = aggregate_count_query(field)
     
-    search_results_hash = search(token,query)
+    begin
+      search_results_hash = search(token,query)
 
-    buckets = search_results_hash.aggregations.field_count.buckets
+      buckets = search_results_hash.aggregations.field_count.buckets
 
-    buckets.map {|b| {b['key'] => b.doc_count} }
+      buckets.map {|b| {b['key'] => b.doc_count} }
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      nil
+    end
   end
 
   def cardinal_count(field, unique_field)
     query = cardinal_count_query(field, unique_field)
-    
-    search_results_hash = search(token,query)
 
-    buckets = search_results_hash.aggregations.field_count.buckets
+    begin
+      search_results_hash = search(token,query)
 
-    buckets.map {|b| {b['key'] => b.unique_field.value} }
+      buckets = search_results_hash.aggregations.field_count.buckets
+
+      buckets.map {|b| {b['key'] => b.unique_field.value} }
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      nil
+    end
   end
 
   def count_over_time(field, interval)
     query = count_over_time_query(field, interval)
-    p query
-    search_results_hash = search(token,query)
-    buckets = search_results_hash.aggregations.field_count.buckets
-    buckets.map do |b| 
-      results_per_minute = b.results_per_minute.buckets.map do |r|
-        p r
-        { r['key'] => r.doc_count }
+    begin
+      search_results_hash = search(token,query)
+      buckets = search_results_hash.aggregations.field_count.buckets
+      buckets.map do |b| 
+        results_per_minute = b.results_per_minute.buckets.map do |r|
+          { r['key'] => r.doc_count }
+        end
+        {b['key'] => results_per_minute }
       end
-      {b['key'] => results_per_minute }
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      nil
     end
   end
-
-
 end
+
