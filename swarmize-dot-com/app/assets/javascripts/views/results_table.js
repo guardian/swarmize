a7159187745cc627f9becb02aa458fbc8aa6825c
Tom Armitage <tom@infovore.org>
2014-10-07 11:50:33 +0100

    Catch a ResultsTable object that doesn't have any rows set.

--- swarmize-dot-com/app/assets/javascripts/views/results_table.js
+++ swarmize-dot-com/app/assets/javascripts/views/results_table.js
@@ -5,24 +5,26 @@ var ResultsTable = Backbone.View.extend({
 
   render: function() {
     var that = this;
-    $("#results_table tbody tr").remove();
-    $.each(this.model.get('rows'), function(i,item) {
-      var $row = $('<tr>');
-      $.each(that.model.get('fieldCodes'), function(j, code) {
-        if(code == 'timestamp') {
-          var rawTime = item[code];
-          var formattedTime = formatTimestamp(parseTimestamp(rawTime));
-          $row.append(
-            $('<td>').text(formattedTime)
-          );
-        } else {
-          $row.append(
-            $('<td>').text(item[code])
-          );
-        }
+    if(this.model.has('rows')) {
+      $("#results_table tbody tr").remove();
+      $.each(this.model.get('rows'), function(i,item) {
+        var $row = $('<tr>');
+        $.each(that.model.get('fieldCodes'), function(j, code) {
+          if(code == 'timestamp') {
+            var rawTime = item[code];
+            var formattedTime = formatTimestamp(parseTimestamp(rawTime));
+            $row.append(
+              $('<td>').text(formattedTime)
+            );
+          } else {
+            $row.append(
+              $('<td>').text(item[code])
+            );
+          }
+        });
+        $("#results_table tbody").append($row);
       });
-      $("#results_table tbody").append($row);
-    });
+    }
     $("span.huge-number").text(this.model.get('count'));
   }
 });
