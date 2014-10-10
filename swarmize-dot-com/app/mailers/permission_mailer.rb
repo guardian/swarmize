722fa1567d65e0cfbcb9c834d9c9430072df9061
Tom Armitage <tom@infovore.org>
2014-10-10 11:05:17 +0100

    Setting a returnpath on the permission mailer.

--- swarmize-dot-com/app/mailers/permission_mailer.rb
+++ swarmize-dot-com/app/mailers/permission_mailer.rb
@@ -1,6 +1,7 @@
 class PermissionMailer < ActionMailer::Base
   default from: "noreply@swarmize.com",
-          reply_to: 'graham.tackley@guardian.co.uk'
+          reply_to: 'graham.tackley@guardian.co.uk',
+          return_path: 'graham.tackley@guardian.co.uk'
 
   def permission_email(to_address, swarm)
     @swarm = swarm
