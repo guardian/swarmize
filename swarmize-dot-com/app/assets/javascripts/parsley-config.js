a8a1382f9a4d6ba8d2d7c6f13fe5c8085bc1e6ec
Tom Armitage <tom@infovore.org>
2014-10-17 12:11:13 +0100

    That works for pick_one.

--- swarmize-dot-com/app/assets/javascripts/parsley-config.js
+++ swarmize-dot-com/app/assets/javascripts/parsley-config.js
@@ -11,11 +11,22 @@ window.ParsleyConfig = {
         }
       },
       priority: 32
+    },
+    conditionalother: {
+      fn: function (value, requirement) {
+        if(($("input[name='" + requirement + "']:checked").val() == 'other') && value == '') {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      priority: 32
     }
   },
   i18n: {
     en: {
-      postcode: 'Please enter a valid UK postcode, eg "SW1A 1AA" or "SW1A"'
+      postcode: 'Please enter a valid UK postcode, eg "SW1A 1AA" or "SW1A"',
+      conditionalother: 'Please supply an answer for \'other\''
     }
   }
 };
