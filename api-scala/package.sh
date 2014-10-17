fa3aeee872c76debbe2a969ad6dbd7ad3acdc70f
Graham Tackley <graham@tackley.net>
2014-10-17 10:50:20 +0100

    Deployable scala api

---
+++ api-scala/package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PACKAGE_FILENAME=$1
+DIRECTORY=$2
+
+APP_NAME=api
+
+# clear up stuff left behind from previous builds
+[ -d target/docker ] && rm -rf target/docker
+
+# build the zip
+cd ..
+
+sbt ${APP_NAME}/docker:stage && \
+  cd ${DIRECTORY}/target/docker && \
+  zip -r ../../${PACKAGE_FILENAME} *
+
+
