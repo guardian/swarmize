7d2b9f80248c0e3adaac357943d87c02421ee42b
Tom Armitage <tom@infovore.org>
2014-10-20 18:25:18 +0100

    Legibility and comments.

--- swarmize-dot-com/app/assets/javascripts/embed.js
+++ swarmize-dot-com/app/assets/javascripts/embed.js
@@ -4,7 +4,7 @@
 //= require parsley.min
 
 function emitHeight() {
-  // get the height of this, and send it to the parent + 300 or something.
+  // get the height of this, and send it to the parent 
   var thisHeight = $(".container").height();
 
   // pipe-delimted data:
@@ -22,24 +22,29 @@ $(document).ready(function() {
   $( "form" ).on( "submit", function( event ) {
     var form = this;
     event.preventDefault();
+
     $.ajax({
       url: $(form).attr('action'),
       data: $(form).serialize(),
       type: 'POST'
     }).done(function(data) {
       $(".embeddable-swarm form").remove();
+
       var resultDiv = $("<div>");
       resultDiv.addClass('result').addClass('success');
       resultDiv.text("Thank you for your submission.");
       $(".embeddable-swarm .feedback").append(resultDiv);
+
       emitHeight();
       parent.postMessage('formsuccess', '*'); 
     }).fail(function(data) {
       $("div.failure").remove();
+
       var resultDiv = $("<div>");
       resultDiv.addClass('result').addClass('failure');
       resultDiv.text("We're sorry: there was an error, and your results could not be submitted.");
       $(".embeddable-swarm form p.submit").prepend(resultDiv);
+
       emitHeight();
     });
   });
--- swarmize-dot-com/app/assets/javascripts/swarmize-embed.js
+++ swarmize-dot-com/app/assets/javascripts/swarmize-embed.js
@@ -1,29 +1,34 @@
-// this is the js to embed in a page
-// it must assume NO JQUERY
+// This is the JS embedded in the page that will host the iFrame
+// It assumes that there is no jQuery, just a modern browser.
 
 // when we're good to go
 window.onload = function() {
   // find the div
   var embedDiv = document.getElementById('swarmize-embedded-form');
 
+  // extract token from data attribute
   var token = embedDiv.dataset.swarmizeToken;
 
-  // insert an iFrame
+  // insert an iFrame containing the embed
   embedDiv.innerHTML = "<iframe src='http://cdn.swarmize.com/swarms/"+token+"/embed' width='460' height='400' frameBorder='0' seamless='seamless'></iframe>";
 
   var iframe = embedDiv.getElementsByTagName('iframe')[0];
   
   respondToMessage = function(e) {
     var chunks = e.data.split("|");
+
+    // when the form changes size, update the height of the iFrame
     if(chunks[0] == 'setHeight') {
       var height = chunks[1];
       iframe.setAttribute('height', height);
     }
 
+    // when the form is submitted, scroll top the top of the iFrame
     if(chunks[0] == 'formsuccess') {
       window.scrollTo(0,iframe.offsetTop);
     }
   }
-  // we have to listen for 'message'
+
+  // listen out for the message event.
   window.addEventListener('message', respondToMessage, false);
 }
