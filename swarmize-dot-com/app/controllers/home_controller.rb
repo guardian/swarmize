01d355090100c5e5e882163f9efe24d4a7b75b53
Tom Armitage <tom@infovore.org>
2014-10-21 16:10:25 +0100

    These should still be calculated.

--- swarmize-dot-com/app/controllers/home_controller.rb
+++ swarmize-dot-com/app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
       @user_swarms_draft = @current_user.swarms.draft.latest(5)
       @user_swarms_in_progress = @current_user.swarms.live.latest(5)
       @user_completed_swarms = @current_user.swarms.closed.latest(5)
+      @swarms_in_progress = Swarm.live.latest(5)
+      @latest_complete_swarms = Swarm.closed.latest(5)
     end
   end
 end
