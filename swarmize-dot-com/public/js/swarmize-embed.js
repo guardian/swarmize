107b57c6df947f9bc97841d618f6f8a17d368d1a
Tom Armitage <tom@infovore.org>
2014-12-08 13:10:50 +0000

    Fixing these up for live.

--- swarmize-dot-com/public/js/swarmize-embed.js
+++ swarmize-dot-com/public/js/swarmize-embed.js
@@ -6,36 +6,38 @@ window.onload = function() {
   // find the div
   var embedDiv = document.getElementById('swarmize-embedded-form');
 
-  // extract token from data attribute
-  var token = embedDiv.dataset.swarmizeToken;
+  if(embedDiv) {
+    // extract token from data attribute
+    var token = embedDiv.dataset.swarmizeToken;
 
-  // insert an iFrame containing the embed
-  if((window.location.hostname == 'alpha.swarmize.com') || (window.location.hostname == 'www.swarmize.com')) {
-    var origin = window.location.origin;
-  } else {
-    var origin = 'http://cdn.swarmize.com';
-  }
-  embedDiv.innerHTML = "<iframe src='" + origin + "/swarms/"+token+"/embed' width='460' height='400' frameBorder='0' seamless='seamless'></iframe>";
+    // insert an iFrame containing the embed
+    if((window.location.hostname == 'alpha.swarmize.com') || (window.location.hostname == 'www.swarmize.com') || (window.location.hostname == 'localhost')) {
+      var origin = window.location.origin;
+    } else {
+      var origin = 'http://cdn.swarmize.com';
+    }
+    embedDiv.innerHTML = "<iframe src='" + origin + "/swarms/"+token+"/embed' width='460' height='400' frameBorder='0' seamless='seamless'></iframe>";
 
-  var iframe = embedDiv.getElementsByTagName('iframe')[0];
+    var iframe = embedDiv.getElementsByTagName('iframe')[0];
 
-  iframe.style.width = '100%';
-  
-  respondToMessage = function(e) {
-    var chunks = e.data.split("|");
+    iframe.style.width = '100%';
 
-    // when the form changes size, update the height of the iFrame
-    if(chunks[0] == 'swarmizeSetHeight') {
-      var height = chunks[1];
-      iframe.setAttribute('height', height);
+    respondToMessage = function(e) {
+      var chunks = e.data.split("|");
+
+      // when the form changes size, update the height of the iFrame
+      if(chunks[0] == 'swarmizeSetHeight') {
+        var height = chunks[1];
+        iframe.setAttribute('height', height);
+      }
+
+      // when the form is submitted, scroll top the top of the iFrame
+      if(chunks[0] == 'swarmizeFormSuccess') {
+        window.scrollTo(0,iframe.offsetTop);
+      }
     }
 
-    // when the form is submitted, scroll top the top of the iFrame
-    if(chunks[0] == 'swarmizeFormSuccess') {
-      window.scrollTo(0,iframe.offsetTop);
-    }
+    // listen out for the message event.
+    window.addEventListener('message', respondToMessage, false);
   }
-
-  // listen out for the message event.
-  window.addEventListener('message', respondToMessage, false);
 }
